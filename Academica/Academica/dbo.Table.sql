5bd82441683861a6b1ba12b050749de71a433a2c
FaridClaros <faridclaros15@gmail.com>
2024-09-10 11:47:55 -0600

    tabla

---
+++ Academica/Academica/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] CHAR(70) NOT NULL, 
+    [Localidad] CHAR(70) NOT NULL, 
+    [Dui] CHAR(10) NULL, 
+    [Telefono] CHAR(10) NULL
+)
