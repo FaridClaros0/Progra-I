987722bc08b076d2d1b4f263aa0b16d2938ab0b3
hola <example@ugb.edu.sv>
2024-09-11 09:30:58 -0600

    ta

--- Academica/Academica/dbo.Table.sql
+++ Academica/Academica/dbo.Table.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Table] (
     [IdAlumno]  INT        NOT NULL,
     [Codigo]    CHAR (10)  NOT NULL,
-    [Nombre]    CHAR (75)  NOT NULL,
+    [Nombre]    CHAR (50)  NOT NULL,
     [Direccion] CHAR (150) NOT NULL,
     [Dui]       CHAR (10)  NULL,
     [Telefono]  CHAR (10)  NULL,
