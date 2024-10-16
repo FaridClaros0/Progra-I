4ecc77ff4095ce4ddd19682040919e8e7a669ef7
FaridClaros <faridclaros15@gmail.com>
2024-10-15 23:14:15 -0600

    conectando db

---
+++ sistema_academica/sistema_academica/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sistema_academica.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int IdAlumno { get; set; }
+        public string Codigo { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
